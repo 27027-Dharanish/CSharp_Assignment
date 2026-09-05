54d9928c4bcad5e59df77eb0c0c3768bc093f949
27027-Dharanish <dharanish.somu@solitontech.com>
2026-09-05 17:25:27 +0530

    fix:resolved EOL error

--- src/MemoryManagement/View/ConsoleActivity.cs
+++ src/MemoryManagement/View/ConsoleActivity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MemoryManagement.View
+﻿namespace MemoryManagement.View
 {
     /// <summary>
     /// Handles user interaction activities by managing standard input and output via the console.
