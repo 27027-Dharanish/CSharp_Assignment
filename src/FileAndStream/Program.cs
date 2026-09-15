0ba2595278d9678bf9360df90e3f23f602ef5bce
27027-Dharanish <dharanish.somu@solitontech.com>
2026-09-15 18:32:39 +0530

    feat:implement task 1

---
+++ src/FileAndStream/Program.cs
@@ -0,0 +1,16 @@
+﻿using FileAndStream;
+
+namespace Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1 task1 = new Task1();
+            task1.GenerateOneGBFile();
+            task1.ReadFileUsingFileStream();
+            task1.ReadFileUsingBufferedStream();
+            task1.ConvertUpperCase();
+        }
+    }
+}
